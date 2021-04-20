08dc499e87c4e77798432e51c15b48821650b7e0
Mandar Vaze <mandarvaze@gmail.com>
2021-04-20 22:57:52 +0530

    explore data files

--- README.txt
+++ README.txt
@@ -1,3 +1,24 @@
+# Requirements
+
+- Docker
+- Python 3.8
+
+# Setup
+
+```shell
 python -m venv .venv-prometheus
 source .venv-prometheus/bin/activate
 pip install -r requirements.txt
+pre-commit install  # Only for development
+```
+
+# Start postgres
+
+`make start-pgsql` (To start in the background)
+`make pgsql` (To start in the foreground - sometimes this is helpful to troubleshoot)
+
+Use `docker ps` to ensure that postgres has actually started
+
+# Assumptions
+
+The data files are under `data` folder.
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import ndjson
 from base import engine, Base
-from models import Patient, Procedure, Encounter, Procedure
+from models import Patient, Procedure, Encounter, Observation  # noqa
+
+
+def import_data(datafile):
+    print(datafile)
+    count = 0
+    with open(datafile) as f:
+        reader = ndjson.reader(f)
+        for line in reader:
+            print(line.keys())
+            count += 1
+            break
+
+    print(count)
 
 
 def main():
     Base.metadata.create_all(engine)
+    import_data('./data/Patient.ndjson')
+    import_data('./data/Encounter.ndjson')
+    import_data('./data/Procedure.ndjson')
+    import_data('./data/Observation.ndjson')
 
 
 if __name__ == '__main__':
